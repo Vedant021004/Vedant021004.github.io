16d99975165113e1aaf3074d7c7207b5f91d484a
Vedant021004 <vedantkp79@gmail.com>
2026-07-23 21:05:09 +0530

    style: fix ant text clipping by centering it

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -140,7 +140,7 @@ export const Home = () => {
                       initial={{ opacity: 0, scale: 0, y: 10 }}
                       animate={{ opacity: 1, scale: 1, y: 0 }}
                       transition={{ delay: 2, duration: 0.5, type: "spring" }}
-                      className="absolute -top-10 -right-4 text-[#ff9500] text-2xl md:text-4xl font-black drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)] whitespace-nowrap z-50"
+                      className="absolute -top-12 left-1/2 -translate-x-1/2 text-[#ff9500] text-2xl md:text-4xl font-black drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)] whitespace-nowrap z-50"
                     >
                       Hi! 👋
                     </motion.div>
